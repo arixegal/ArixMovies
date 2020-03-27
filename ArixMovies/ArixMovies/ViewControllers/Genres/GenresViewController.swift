0b9e59e9fa998a499998bd117242f90ff4eaa2e1
Arik Segal <arikisintown@gmail.com>
2020-03-27 20:20:09 +0300

    removed unused code

--- ArixMovies/ArixMovies/ViewControllers/Genres/GenresViewController.swift
+++ ArixMovies/ArixMovies/ViewControllers/Genres/GenresViewController.swift
@@ -18,7 +18,6 @@ class GenresViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        constCollectionViewTop.constant = -900
         fetchGenres()
     }
 }
