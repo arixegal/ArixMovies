f5eb16df6fe116f23077f3eee210dc21da7eac16
Arik Segal <arikisintown@gmail.com>
2020-03-27 15:33:59 +0300

    added genres ids to movie model

--- ArixMovies/ArixMovies/Model/Movie.swift
+++ ArixMovies/ArixMovies/Model/Movie.swift
@@ -13,7 +13,7 @@ struct Movie: Decodable {
     let title: String?
     let poster_path: String?
     let overview: String?
-    let releaseDate: String?
-    let backdrop_path: String?
     let release_date: String?
+    let backdrop_path: String?
+    let genre_ids: [Int]?
 }
